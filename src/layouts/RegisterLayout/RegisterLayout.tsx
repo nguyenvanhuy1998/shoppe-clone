b9ce897dc52118b9d8cef7cc9bd7718b08f9927c
huynguyen <huy.nguyenvan@mafc.com.vn>
2023-09-05 16:55:52 +0700

    Setup react router

---
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  children?: React.ReactNode
+}
+function RegisterLayout({ children }: Props) {
+  return (
+    <div>
+      RegisterLayout
+      {children}
+    </div>
+  )
+}
+
+export default RegisterLayout
