69afa4fc06df8341631ce5a9bd05ff05be82b8dc
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2023-09-26 11:15:56 +0700

    Hien thi product va format number voi Intl

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -15,7 +15,6 @@ export default function ProductList() {
     }
   })
   console.log(data)
-  console.log(queryParams)
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
@@ -28,11 +27,10 @@ export default function ProductList() {
           <div className='col-span-9'>
             <SortProductList />
             <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-              {Array(30)
-                .fill(0)
-                .map((_, index) => (
-                  <div className='col-span-1' key={index}>
-                    <Product />
+              {data &&
+                data.data.data.products.map((product) => (
+                  <div className='col-span-1' key={product._id}>
+                    <Product product={product} />
                   </div>
                 ))}
             </div>
